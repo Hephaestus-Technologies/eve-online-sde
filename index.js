50d79fc44c32c40109a57f3bfba495fc0fa4e858
Nathan Floris Copier <nfcopier1@aggiemail.usu.edu>
2020-09-09 21:21:16 -0600

    Removes some unnecessary stuff
    Project is now just a thin adapter around the file system

--- index.js
+++ index.js
@@ -28,33 +28,3 @@ exports.region = async (name) => {
         return await loadFile("fsd", "universe", "wormhole", name, "region");
     }
 };
-
-exports.lookup = async (query, lang = "en") => {
-    return Object
-        .entries(await exports.types())
-        .filter(matches(query, lang))
-        .map(([id, value]) => ({...value, id: Number(id)}));
-};
-
-const matches = (query, lang) => ([, type]) => {
-    return type.name && type.name[lang] && type.name[lang].includes( query );
-};
-
-exports.typesForCategory = async (category) => {
-    const [groups, types] = await Promise.all([exports.groups(), exports.types()]);
-    const groupIds = Object
-        .entries(groups)
-        .filter(([,g]) => g.categoryID === category)
-        .map(([id]) => id);
-    return Object
-        .entries(types)
-        .filter(([,t]) => groupIds.includes(t.groupID))
-        .map((id, t) => ({id: Number(id), ...t}));
-};
-
-exports.lookupById = async (id) => {
-    const types = await exports.types();
-    const result = types[id];
-    if (!result) return null;
-    return {...result, id: Number(id)};
-};
