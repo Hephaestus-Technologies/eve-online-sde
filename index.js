0500a21120fd33edd8b780958331d13ec091544f
Nathan Floris Copier <nfcopier1@aggiemail.usu.edu>
2020-09-09 22:58:14 -0600

    Adds some additional routes for the larger datasets

--- index.js
+++ index.js
@@ -4,21 +4,59 @@ const loadFile = require("./load_file");
 const PORT = 60606;
 const app = Express();
 
-const GET = (route, handler) => {
-    app.get(route, async (req, res) => res.send(await handler(req.params)));
+const getAllGroups = () => loadFile("fsd", "groupIDs");
+
+const getAllTypes = () => loadFile("fsd", "typeIDs");
+
+const getAllBlueprints = () => loadFile("fsd", "blueprints");
+
+const getTypesFor = async (category) => {
+    const [groups, types] = await Promise.all([getAllGroups(), getAllTypes()]);
+    const groupIds = groupIdsFor(category, groups);
+    return typesForGroupIds(groupIds, types);
 };
 
-GET("/types", () => loadFile("fsd", "typeIDs"));
+const groupIdsFor = (category, groups) => {
+    return Object
+        .entries(groups)
+        .filter(([, g]) => g.categoryID === category)
+        .map(([id]) => Number(id));
+};
+
+const typesForGroupIds = (groupIds, types) => {
+    return Object
+        .entries(types)
+        .filter(([, t]) => groupIds.includes(t.groupID))
+        .map(entityFromEntry)
+};
+
+const entityById = async (entityId, getAllEntities) => {
+    const entities = await getAllEntities();
+    const entity = entities[entityId];
+    return entity ? entityFromEntry([entityId, entity]) : null;
+};
+
+const entityFromEntry = ([entityId, value]) => ({entityId: Number(entityId), ...value});
+
+const GET = (route, handler) => {
+    app.get(route, async (req, res) => res.send(await handler(req.params, req.query)));
+};
+
+GET("/types/:typeId", ({typeId}) => entityById(Number(typeId), getAllTypes));
+
+GET("/types", (_, {category}) => category == null ? getAllTypes() : getTypesFor(Number(category)));
 
 GET("/icons", () => loadFile("fsd", "iconIDs"));
 
-GET("/blueprints", () => loadFile("fsd", "blueprints"));
+GET("/blueprints/:blueprintId", ({blueprintId}) => entityById(Number(blueprintId), getAllBlueprints));
+
+GET("/blueprints", getAllBlueprints);
 
 GET("/certificates", () => loadFile("fsd", "certificates"));
 
 GET("/graphics", () => loadFile("fsd", "graphicIDs"));
 
-GET("/groups", () => loadFile("fsd", "groupIDs"));
+GET("/groups", getAllGroups);
 
 GET("/skinMaterials", () => loadFile("fsd", "skinMaterials"));
 
